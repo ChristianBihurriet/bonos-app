ceb9d1d7aad1610dc2829a992487d4dc174d943d
Christian Bihurriet <christian.00@hotmail.com.ar>
2026-04-03 16:33:58 +0100

    feat(app): inicialización de SPA con control de sesión y navegación básica

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+// main.js
+import { renderLogin } from "./ui.js";
+import { cargarBonos } from "./bonos.js";
+
+function init() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    cargarBonos();
+  } else {
+    renderLogin();
+  }
+}
+
 init();
